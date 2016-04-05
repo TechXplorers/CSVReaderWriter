aec2ca82576ed5f2e13caa825b85b7b3aac78e11
TechXplorers <techxplorers.uk@gmail.com>
2016-04-05 11:02:12 +0100

    Added files via upload

---
+++ CSVReaderWriter.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.IO;
+
+namespace Symphony
+{
+    /// <summary>
+    /// A junior developer was tasked with writing a reusable solution for an application to read and write text files that hold tab separated data.
+    /// His implementation, although it works and meets the needs of the application, is of very low quality.
+    /// Your task:
+    ///     -  Identify and annotate the shortcomings in the current implementation as if you were doing a code review, using comments in this file.
+    ///     -  In a fresh solution, refactor this implementation into clean, elegant, rock-solid & well performing code, without over-engineering.
+    ///     -  Where you make trade offs, comment & explain.”
+    ///     -  Assume this code is in production and it needs to be backwards compatible. Therefore if you decide to change the public interface, 
+    ///        please deprecate the existing methods. Feel free to evolve the code in other ways though.
+
+    /// </summary>
+    //You should implement interface ICSVReaderWriter for IoC and unit testing
+    //You should add comments on all methods
+    public class CSVReaderWriter
+    {
+        private StreamReader _readerStream = null;
+        private StreamWriter _writerStream = null;
+
+        //This enum should not be marked as Flags. There is no bitwise operation to be performed. There also is no need for this enum at all. See comments on Open method.
+        [Flags]
+        public enum Mode { Read = 1, Write = 2 };
+
+        //There should be no need to call Open method twice to create reader and writer. There should be two arguments for inputFile and outputFile in a constructor with outputfile being empty.
+        public void Open(string fileName, Mode mode)
+        {
+            if (mode == Mode.Read)
+            {
+                _readerStream = File.OpenText(fileName);
+            }
+            else if (mode == Mode.Write)
+            {
+                //Why do you need to create FileInfo first?
+                FileInfo fileInfo = new FileInfo(fileName);
+                _writerStream = fileInfo.CreateText();
+            }
+            else
+            {
+                throw new Exception("Unknown file mode for " + fileName);
+            }
+        }
+
+        public void Write(params string[] columns)
+        {
+            //depending on company standards, should be "", string.Empty or String.Empty. There is no difference in performance or memory management but it's good to keep standards
+            string outPut = "";
+
+            //instead of joining string in a loop it's better to use String.Join("\t", columns);
+            for (int i = 0; i < columns.Length; i++)
+            {
+                //It's better to use StringBuiler. Saves memory, important especially for big files
+                outPut += columns[i];
+                if ((columns.Length - 1) != i)
+                {
+                    outPut += "\t";
+                }
+            }
+
+            WriteLine(outPut);
+        }
+
+        //Why do you limit this method to two columns, when in "Write" method columns are as params?
+        //If parameters of this method are strings, this method only checks if the line could be read and had two columns in line
+        public bool Read(string column1, string column2)
+        {
+            //What are those consts for? What if we'll need three or more columns?
+            const int FIRST_COLUMN = 0;
+            const int SECOND_COLUMN = 1;
+
+            //There is no need to declare those variables here. You should do that while assigning values
+            string line;
+            string[] columns;
+
+            //This separator should be set as method parameter or put directly in Split method
+            char[] separator = { '\t' };
+
+            line = ReadLine();
+            //if ReadLine will return null, there will be an unhandled exception here
+            columns = line.Split(separator);
+
+            //If we have one or more than two columns in a file, is it an error?
+            if (columns.Length == 2)
+            {
+                //These assignments are pointess
+                column1 = columns[FIRST_COLUMN];
+                column2 = columns[SECOND_COLUMN];
+
+                return true;
+            }
+            else if (columns.Length == 0)
+            {
+                //These assignments are pointess
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+
+            //you should create a custom exception
+            throw new Exception("Expected 2 columns in csv input file");
+        }
+
+        //If a file format will change (e.g. new column added), new method would have to be added. This one should be refactored and return array of strings.
+        public bool Read(out string column1, out string column2)
+        {
+            //those consts are not needed, amount of columns should be taken from method parameter or from file content
+            const int FIRST_COLUMN = 0;
+            const int SECOND_COLUMN = 1;
+
+            //There is no need to declare those variables here. You should do that while assigning values
+            string line;
+            string[] columns;
+
+            //This separator should be set as method parameter or put directly in Split method
+            char[] separator = { '\t' };
+
+            line = ReadLine();
+
+            if (line == null)
+            {
+                //return null. See below.
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+
+            columns = line.Split(separator);
+
+            //if you would add int expectedColumnnsNumber as a method parameter, all you need to do here is to check if columns.Length == expectedColumnnsNumber. If yes, return columns, if no, return null
+            if (columns.Length == 2)
+            {
+                column1 = columns[FIRST_COLUMN];
+                column2 = columns[SECOND_COLUMN];
+
+                return true;
+            }
+            else if (columns.Length == 0)
+            {
+                column1 = null;
+                column2 = null;
+
+                return false;
+            }
+
+            //you should create a custom exception
+            throw new Exception("Expected 2 columns in csv input file");
+        }
+
+        //This method should be public, but not in an interface. This would allow mocking for unit tests
+        private void WriteLine(string line)
+        {
+            //what if _writerStream is null?
+            _writerStream.WriteLine(line);
+        }
+
+        //This method should be public, but not in an interface. This would allow mocking for unit tests
+        private string ReadLine()
+        {
+            //what if _readerStream is null?
+            return _readerStream.ReadLine();
+        }
+
+        //What if no one calls this method? The strams will stay open. This class should implement IDisposable and you should close streams there
+        public void Close()
+        {
+            if (_writerStream != null)
+            {
+                _writerStream.Close();
+            }
+
+            if (_readerStream != null)
+            {
+                _readerStream.Close();
+            }
+        }
+    }
+}
---
+++ CSVReaderWriterNew.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//I didn't set any method as deprecated as all of them would be, and I didn't want to copy the code or fix it not to obscure my solution. Please, let me know if you want me to.
+namespace Symphony
+{
+    public interface ICSVReaderWriterNew
+    {
+        /// <summary>
+        /// Reads a line from a file. Splits with a delimeter set in constructor. Defaults to '\t'
+        /// </summary>
+        /// <param name="expectedColumnsNumber">Optional parameter. Number of columns expected in a line being read. </param>
+        /// <returns>Array of strings from columns in a line</returns>
+        /// <exception cref="IncorrectColumnsNumberException">Thrown if a number of columns is different and not zero</exception>
+        string[] Read(int expectedColumnsNumber = 0);
+
+        /// <summary>
+        /// Writes to a file a line with delimited values from columns array
+        /// </summary>
+        /// <param name="columns">Columns to be written</param>
+        /// <exception cref="WriterNotConfiguredException">Thrown when writer is not being set up</exception>
+        void Write(params string[] columns);
+
+        /// <summary>
+        /// Writes to a specified StreamWriter a line with delimited values from columns array
+        /// </summary>
+        /// <param name="writer">WriterStream where values will be written to</param>
+        /// <param name="columns">Columns to be written</param>
+        void Write(StreamWriter writer, params string[] columns);
+    }
+
+    public class CSVReaderWriterNew : ICSVReaderWriterNew, IDisposable
+    {
+        private StreamReader _readerStream = null;
+        private StreamWriter _writerStream = null;
+        private const char _defaultDelimeter = '\t';
+        private char _delimeter;
+
+        /// <summary>
+        /// Creates new instance of CSVReaderWriterNew. All arguments are obligatory
+        /// </summary>
+        /// <param name="reader">Reader stream</param>
+        /// <param name="writer">Writer stream</param>
+        /// <param name="delimeter">Delimeter</param>
+        public CSVReaderWriterNew(StreamReader reader, StreamWriter writer, char delimeter)
+        {
+            if (reader == null)
+            {
+                throw new ArgumentException("Parameter reader must not be null");
+            }
+            if (writer == null)
+            {
+                throw new ArgumentException("Parameter writer must not be null");
+            }
+            if (Char.IsWhiteSpace(delimeter))
+            {
+                throw new ArgumentException("Parameter delimeter must not be white space");
+            }
+
+            _readerStream = reader;
+            _writerStream = writer;
+            _delimeter = delimeter;
+        }
+
+        /// <summary>
+        /// Creates new instance of CSVReaderWriterNew.
+        /// </summary>
+        /// <param name="inputFile">Input file name</param>
+        /// <param name="outputFile">Output file name</param>
+        /// <param name="delimeter">Delimeter</param>
+        public CSVReaderWriterNew(string inputFile, string outputFile, char delimeter)
+        {
+            if (String.IsNullOrEmpty(inputFile))
+            {
+                throw new ArgumentException("Parameter inputFile must not be null or empty");
+            }
+
+            _readerStream = _readerStream = File.OpenText(inputFile);
+            _delimeter = delimeter;
+
+            if (!String.IsNullOrEmpty(outputFile))
+            {
+                _writerStream = new StreamWriter(outputFile);
+            }
+        }
+
+        /// <summary>
+        /// Creates new instance of CSVReaderWriterNew. Default delimeter is '\t'
+        /// </summary>
+        /// <param name="inputFile">Input file name</param>
+        /// <param name="outputFile">Output file name</param>
+        public CSVReaderWriterNew(string inputFile, string outputFile)
+            : this(inputFile, outputFile, _defaultDelimeter)
+        {
+        }
+
+        /// <summary>
+        /// Creates new instance of CSVReaderWriterNew.
+        /// </summary>
+        /// <param name="inputFile">Input file name</param>
+        /// <param name="delimeter">Delimeter</param>
+        public CSVReaderWriterNew(string inputFile, char delimeter)
+            : this(inputFile, String.Empty, delimeter)
+        {
+        }
+
+        /// <summary>
+        /// Creates new instance of CSVReaderWriterNew. Default delimeter is '\t'
+        /// </summary>
+        /// <param name="inputFile">Input file name</param>
+        public CSVReaderWriterNew(string inputFile)
+            : this(inputFile, String.Empty, _defaultDelimeter)
+        {
+        }
+
+        public string[] Read(int expectedColumnsNumber = 0)
+        {
+            var line = _readerStream.ReadLine();
+
+            if (line == null)
+            {
+                return null;
+            }
+
+            var tokens = line.Split(_delimeter);
+
+            if (expectedColumnsNumber != 0 && tokens.Length != expectedColumnsNumber)
+            {
+                throw new IncorrectColumnsNumberException();
+            }
+
+            return tokens;
+        }
+
+        public void Write(params string[] columns)
+        {
+            if (_writerStream == null)
+            {
+                throw new WriterNotConfiguredException();
+            }
+
+            Write(_writerStream, columns);
+        }
+
+        //I would consider passing StreamWriter. For example depending on line content you might want to write to a different stream.
+        public void Write(StreamWriter writer, params string[] columns)
+        {
+            var text = String.Join("\t", columns);
+
+            writer.WriteLine(text);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IncorrectColumnsNumberException : Exception
+    { }
+
+    public class WriterNotConfiguredException : Exception
+    { }
+}
